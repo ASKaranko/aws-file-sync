3e9b1c09f32bf6d39c50473bf5116d1753b7cc94
Andrei Karanko <a.karanko@polytech.software>
2025-05-20 15:04:40 +0300

    Adds SQS queue and file processor Lambda integration
    Introduces an SQS queue with dead-letter support and configures a new Lambda to process messages from the queue, enabling asynchronous file processing. Connects the API to the queue via an existing Lambda function and improves logging structure for better monitoring and troubleshooting.

--- lib/file-sync-stack.ts
+++ lib/file-sync-stack.ts
@@ -10,9 +10,11 @@ import {
   LogGroupLogDestination,
   AccessLogFormat,
   MethodLoggingLevel,
-  AuthorizationType
+  AuthorizationType,
+  LambdaIntegration
 } from 'aws-cdk-lib/aws-apigateway';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Queue, QueueEncryption, RedrivePermission } from 'aws-cdk-lib/aws-sqs';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 interface FileSyncStackProps extends StackProps {
   stage: string;
@@ -33,8 +35,14 @@ export class FileSyncStack extends Stack {
     Tags.of(this).add('Project', 'file-sync');
     Tags.of(this).add('Environment', stage);
 
-    const fileSyncCreateFnLogGroup = new LogGroup(this, 'FileSyncCreateFnLogGroup', {
+    const fileSyncCreateRouterLogGroup = new LogGroup(this, 'FileSyncCreateRouterLogGroup', {
       logGroupName: `/aws/lambda/${stage}-file-sync-create-router`,
+      retention: RetentionDays.ONE_MONTH,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
+    const fileProcessorLogGroup = new LogGroup(this, 'FileProcessorLogGroup', {
+      logGroupName: `/aws/lambda/${stage}-file-processor`,
       retention: RetentionDays.ONE_MONTH,
       removalPolicy: RemovalPolicy.DESTROY
     });
@@ -46,14 +54,25 @@ export class FileSyncStack extends Stack {
     });
 
     const fileSyncCreateRouterLambda = new NodejsFunction(this, 'FileSyncCreateRouterLambda', {
-      functionName: `${stage}-file-sync-create-router-lambda`,
+      functionName: `${stage}-file-sync-create-router`,
       runtime: Runtime.NODEJS_22_X,
       architecture: Architecture.X86_64,
       entry: path.join(__dirname, '../lambda/api/create-files.js'),
       handler: 'handler',
       memorySize: 256,
       timeout: Duration.seconds(20),
-      logGroup: fileSyncCreateFnLogGroup
+      logGroup: fileSyncCreateRouterLogGroup
+    });
+
+    const fileProcessorLambda = new NodejsFunction(this, 'FileProcessorLambda', {
+      functionName: `${stage}-file-processor`,
+      runtime: Runtime.NODEJS_22_X,
+      architecture: Architecture.X86_64,
+      entry: path.join(__dirname, '../lambda/transfer/file-processor.js'),
+      handler: 'handler',
+      memorySize: 512,
+      timeout: Duration.seconds(45),
+      logGroup: fileProcessorLogGroup
     });
 
     const api = new LambdaRestApi(this, 'FileSyncApi', {
@@ -81,11 +100,61 @@ export class FileSyncStack extends Stack {
 
     // Add /file-sync resource with POST method
     const fileSyncApiResource = api.root.addResource('file-sync');
-    fileSyncApiResource.addMethod('POST');
+
+    fileSyncApiResource.addMethod(
+      'POST',
+      new LambdaIntegration(fileSyncCreateRouterLambda, {
+        proxy: true,
+        allowTestInvoke: true
+      })
+    );
 
     new CfnOutput(this, 'ApiEndpoint', {
       value: api.url,
       description: 'The URL of the API Gateway endpoint'
+    });
+
+    const fileSyncDLQ = new Queue(this, 'FileSyncDLQ', {
+      queueName: `${stage}-file-sync-dlq`,
+      encryption: QueueEncryption.SQS_MANAGED,
+      retentionPeriod: Duration.days(14),
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
+    const fileSyncQueue = new Queue(this, 'FileSyncQueue', {
+      queueName: `${stage}-file-sync-queue`,
+      encryption: QueueEncryption.SQS_MANAGED,
+      deliveryDelay: Duration.seconds(0),
+      visibilityTimeout: Duration.seconds(270), // best practice is greater that 6 × function timeout
+      receiveMessageWaitTime: Duration.seconds(20),
+      retentionPeriod: Duration.minutes(5),
+      maxMessageSizeBytes: 262144, // 256KB
+      deadLetterQueue: {
+        queue: fileSyncDLQ,
+        maxReceiveCount: 3
+      },
+      redriveAllowPolicy: {
+        redrivePermission: RedrivePermission.DENY_ALL
+      },
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
+    // Grant the Lambda function permission to send messages to the queue
+    fileSyncQueue.grantSendMessages(fileSyncCreateRouterLambda);
+    fileSyncCreateRouterLambda.addEnvironment('FILE_SYNC_QUEUE_URL', fileSyncQueue.queueUrl);
+
+    fileProcessorLambda.addEventSource(
+      new SqsEventSource(fileSyncQueue, {
+        batchSize: 1,
+        maxBatchingWindow: Duration.seconds(0),
+        maxConcurrency: 8,
+        reportBatchItemFailures: true
+      })
+    );
+
+    new CfnOutput(this, 'FileSyncQueueUrl', {
+      value: fileSyncQueue.queueUrl,
+      description: 'The URL of the File Sync SQS queue'
     });
   }
 }
